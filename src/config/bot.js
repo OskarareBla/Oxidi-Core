2aa1e40edb5b5d0324c36589644908194d35797b
OskarareBla <caspersenmads0@gmail.com>
2026-08-04 01:58:08 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         // Text users will see (example: "Playing /help | Titan Bot")
         name: "OxidizedSmp Community ❤", // required by Discord API, not shown in the client
-        state: "Online",     // this is what people actually see
+        state: "HELLO",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
