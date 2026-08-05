4673e58f0457ea6e530953bb959874a73a1db76e
OskarareBla <caspersenmads0@gmail.com>
2026-08-05 14:51:18 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot")
-        name: "Oxidizedsmp.net ❤",
+        name: "OxidizedSMP.net ❤",
         // Activity type number (0・Playing).
-        type: 0,
+        type: 1,
       },
     ],
   },
